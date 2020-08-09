f7fcba3f4f05bf6dac7a31add9cebfa25d9122ed
Tom <tom@Maxs-MacBook-Pro.local>
2020-08-09 18:31:54 +1000

    Restarting entire assignment, completely cooked it

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get "/books", to: "books#index"
+end
+
+Rails.application.routes.draw do
+  get "/", to: "pages#home", as: "root"
+end
---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+for i in 1..10
+    Book.create(
+        author: Faker::Book.author,
+        title: Faker::Book.title,
+        publisher: Faker::Book.publisher,
+        date_published: Faker::Date.between(from: 200.days.ago, to: Date.today)
+    )
+
+    puts "Created #{i} records"
+end
