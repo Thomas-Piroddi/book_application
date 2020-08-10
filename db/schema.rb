4a46cce7a6339610b327fa1f2a44d026a4edc84b
Tom <tom@Maxs-MacBook-Pro.local>
2020-08-10 12:05:02 +1000

    Added all new .rb files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_09_073058) do
+ActiveRecord::Schema.define(version: 2020_08_10_013247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 2020_08_09_073058) do
     t.string "author"
     t.string "publisher"
     t.date "date_published"
+  end
+
+  create_table "conditions", force: :cascade do |t|
+    t.string "name"
+    t.string "condition"
   end
 
 end
